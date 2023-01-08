dbed096d13cd89302d008645ca0efe6c584c0984
Шевченко Алексей Александрович <Shevchenko.Ale.Ale@sberbank.ru>
2023-01-08 18:37:36 +0400

    add toi/test2

---
+++ toi2022/test2/House.java
@@ -0,0 +1,23 @@
+public class House {
+    private int floor;
+
+    public House(int floor) {
+        if (floor < 1) {
+            throw new Error("У дома не может быть этажей меньше 1");
+        } else {
+            this.floor = floor;
+        }
+    }
+
+    public String toString() {
+        String postfix;
+
+        if (Integer.toString(floor).endsWith("1")) {
+            postfix = " этажом";
+        } else {
+            postfix = " этажами";
+        }
+
+        return "Дом с " + this.floor + postfix;
+    }
+}
