7ee0c963d399af3b155aeec323f11819a8b5a498
Шевченко Алексей Александрович <Shevchenko.Ale.Ale@sberbank.ru>
2023-01-09 09:59:09 +0400

    toi fix

--- toi2022/task0/App.java
+++ toi2022/task0/App.java
@@ -1,19 +1,15 @@
 public class App {
     public static void main(String[] args) {
-        Album album1 = new Album("Кукловод", "Металлов");
-        Album album2 = new Album("Шоссе к ООП", "Асид");
+        House house1 = new House(1);
+        System.out.println(house1);
 
-        Track track1 = new Track("Состояние объектов", "Янг");
-        Track track2 = new Track("Шоссе к ООП");
-        Track track3 = new Track("Одноразовый программист", "Бертон");
+        House house2 = new House(5);
+        System.out.println(house2);
 
-        album2.addTrack(track1);
-        album2.addTrack(track2);
-        album1.addTrack(track3);
+        House house3 = new House(23);
+        System.out.println(house3);
 
-        System.out.println(track1);
-        System.out.println(track2);
-        System.out.println(track3);
-        System.out.println(track1.getAlbumList());
+        House house4 = new House(-3);
+        System.out.println(house4);
     }
 }
--- toi2022/task2/App.java
+++ toi2022/task2/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) {
-        
+        Album album1 = new Album("Кукловод", "Металлов");
+        Album album2 = new Album("Шоссе к ООП", "Асид");
+
+        Track track1 = new Track("Состояние объектов", "Янг");
+        Track track2 = new Track("Шоссе к ООП");
+        Track track3 = new Track("Одноразовый программист", "Бертон");
+
+        album2.addTrack(track1);
+        album2.addTrack(track2);
+        album1.addTrack(track3);
+
+        System.out.println(track1);
+        System.out.println(track2);
+        System.out.println(track3);
+        System.out.println(track1.getAlbumList());
     }
 }
